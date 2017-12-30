32d41759307d6c6c815a5497b1325b6ce9b6e4cb
Ivan Silva <ivansilvahi@outlook.com>
2017-12-29 22:44:57 -0300

    commit

---
+++ Examen/Controllers/ClienteController.cs
@@ -0,0 +1,101 @@
+﻿using Examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class ClienteController : Controller
+    {
+        TIENDAEntities2 cnx;
+
+
+        public ClienteController()
+        {
+            cnx = new TIENDAEntities2();
+        }
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        public ActionResult Guardar(string rut, string nombre, string apellido,string direccion, string tipocliente)
+        {
+
+            Examen.Models.Cliente cliente = new Examen.Models.Cliente
+            {
+
+                Rut = rut,
+                Nombre = nombre,
+                Apellido = apellido,
+                Direccion = direccion,
+                TipoCliente = tipocliente
+
+            };
+
+            cnx.Cliente.Add(cliente);
+            cnx.SaveChanges();
+
+
+            return View("Listado", ListadoClientes());
+        }
+        public ActionResult Listado()
+        {
+
+            return View("Listado", ListadoClientes());
+        }
+        public ActionResult Ficha(int id)
+        {
+
+            return View(BuscarCliente(id));
+        }
+
+        private Cliente BuscarCliente(int id)
+        {
+            Cliente nuevo = new Cliente();
+            foreach (Cliente cliente in cnx.Cliente.ToList())
+            {
+                if (cliente.Id == id)
+                {
+                    nuevo = cliente;
+                }
+            }
+            return nuevo;
+        }
+        public ActionResult Visualizar(int id)
+        {
+            Cliente nuevo = BuscarCliente(id);
+
+            if (nuevo != null)
+            {
+                return View("Ficha", nuevo);
+            }
+            return View("Listado", cnx.Cliente.ToList());
+        }
+
+        public ActionResult Eliminar(int id)
+
+        {
+            cnx.Cliente.Remove(cnx.Cliente.Where(x => x.Id.Equals(id)).First());
+
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.Cliente.ToList());
+
+        }
+
+        private List<Examen.Models.Cliente> ListadoClientes()
+        {
+
+            cnx.Database.Connection.Open();
+
+            List<Examen.Models.Cliente> auto = cnx.Cliente.ToList();
+
+            cnx.Database.Connection.Close();
+
+            return auto;
+        }
+
+    }
+}
---
+++ Examen/Controllers/ProductoController.cs
@@ -0,0 +1,106 @@
+﻿using Examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class ProductoController : Controller
+    {
+        TIENDAEntities2 cnx;
+
+
+        public ProductoController()
+        {
+            cnx = new TIENDAEntities2();
+        }
+
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(string nombre, string codigobarra, string familia, string precio, string descuentotope, string descripcion)
+        {
+
+            Examen.Models.Producto producto = new Examen.Models.Producto
+            {
+  
+                Nombre = nombre,
+                CodigoBarra = codigobarra,
+                Familia = familia,
+                Precio = precio,
+                DescuentoTope = descuentotope,
+                Descripcion = descripcion
+
+            };
+
+            cnx.Producto.Add(producto);
+            cnx.SaveChanges();
+
+            return View("Listado", ListadoProductos());
+        }
+        public ActionResult Listado()
+        {
+
+            return View("Listado", ListadoProductos());
+
+        }
+        public ActionResult Ficha(int id)
+        {
+
+            return View(BuscarProducto(id));
+        }
+
+        private Producto BuscarProducto(int id)
+        {
+            Producto nuevo = new Producto();
+            foreach (Producto producto in cnx.Producto.ToList())
+            {
+                if (producto.Id == id)
+                {
+                    nuevo = producto;
+                }
+            }
+            return nuevo;
+        }
+        public ActionResult Visualizar(int id)
+        {
+            Producto nuevo = BuscarProducto(id);
+
+            if (nuevo != null)
+            {
+                return View("Ficha", nuevo);
+            }
+            return View("Listado", cnx.Producto.ToList());
+        }
+
+        public ActionResult Eliminar(int id)
+
+        {
+            cnx.Producto.Remove(cnx.Producto.Where(x => x.Id.Equals(id)).First());
+
+            cnx.SaveChanges();
+
+            return View("Listado", cnx.Producto.ToList());
+
+        }
+
+
+        private List<Examen.Models.Producto> ListadoProductos()
+        {
+
+            cnx.Database.Connection.Open();
+
+
+            List<Examen.Models.Producto> auto = cnx.Producto.ToList();
+
+            cnx.Database.Connection.Close();
+
+            return auto;
+        }
+
+    }
+}
